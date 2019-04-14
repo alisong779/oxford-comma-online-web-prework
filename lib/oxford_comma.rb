2ee2064251caee80d9e0f595afb8758d807105b2
Alison Golden <alisong779@yahoo.com>
2019-04-14 02:58:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,11 @@
 
 def oxford_comma(array)
-array[0..-2].join(', ') + ", and " + array[-1]
   if array.length == 1
     array.join
   elsif array.length == 2
     array.join(" and ")
   else
-    array
+    array[0..-2].join(', ') + ", and " + array[-1]
   end   
 end
 
